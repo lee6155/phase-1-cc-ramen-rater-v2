3b70f5d34d7129175ec251763c724c8d47cf5492
Evan Lee <evan.lee.615@gmail.com>
2024-04-22 00:06:58 -0400

    core deliverables added

--- src/index.js
+++ src/index.js
@@ -1,29 +1,80 @@
-// index.js
 
-// Callbacks
-const handleClick = (ramen) => {
-  // Add code
-};
+function main () {
+  document.addEventListener('DOMContentLoaded', function displayRamens () {
+    fetch('http://localhost:3000/ramens')
+    .then(resp => resp.json())
+    .then(function(data){
+      let imageSection = document.getElementById('ramen-menu')
+      data.forEach(function(object){
+        let imageTag = document.createElement('img')
+        imageTag.setAttribute('src',`${object.image}`)
+        imageTag.setAttribute('id',`${object.id}`)
+        imageSection.append(imageTag)
+      })
+    })
+  });
 
-const addSubmitListener = () => {
-  // Add code
-}
+  function addSubmitListener () {
+    let form = document.getElementById('new-ramen')
+    form.addEventListener('submit', function(e) {
+      e.preventDefault()
+      const configurationObject = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+          "id": "6",
+          "name": `${e.target[0].value}`,
+          "restaurant": `${e.target[1].value}`,
+          "image": `${e.target[2].value}`,
+          "rating": `${e.target[3].value}`,
+          "comment": `${e.target[4].value}`
+        })
+      }
+      fetch('http://localhost:3000/ramens', configurationObject)
+      .then(resp => resp.json())
+    })
+  }
 
-const displayRamens = () => {
-  // Add code
-};
-
-const main = () => {
-  // Invoke displayRamens here
-  // Invoke addSubmitListener here
+  addSubmitListener()
 }
 
 main()
 
+
+function handleClick () {
+  document.addEventListener('click', function(e) {
+    const select = e.target.closest('img')
+    fetch(`http://localhost:3000/ramens/${select.id}`)
+    .then(resp => resp.json())
+    .then(function(data) {
+      let mainImage = document.getElementsByClassName('detail-image')
+      mainImage[0].src = `${data.image}`
+
+      let foodName = document.getElementsByClassName('name')
+      foodName[0].textContent = `${data.name}`
+
+      let restaurant = document.getElementsByClassName('restaurant')
+      restaurant[0].textContent = `${data.restaurant}`
+
+      let rating = document.getElementById('rating-display')
+      rating.textContent = `${data.rating}`
+
+      let comment = document.getElementById('comment-display')
+      comment.textContent = `${data.comment}`
+    })
+  })  
+}
+
+handleClick()
+
+
 // Export functions for testing
-export {
-  displayRamens,
-  addSubmitListener,
-  handleClick,
-  main,
-};
+// export {
+//   displayRamens,
+//   addSubmitListener,
+//   handleClick,
+//   main,
+// };
