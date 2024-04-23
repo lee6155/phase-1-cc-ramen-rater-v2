3440c922d48517425410f183b67c9bc4779b5194
Evan Lee <evan.lee.615@gmail.com>
2024-04-23 02:49:05 -0400

    added advanced deliverables, some cleanup needed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 
+let mainImage = document.getElementsByClassName('detail-image')
+let foodName = document.getElementsByClassName('name')
+let restaurant = document.getElementsByClassName('restaurant')
+let rating = document.getElementById('rating-display')
+let comment = document.getElementById('comment-display')
+
+
 function main () {
   document.addEventListener('DOMContentLoaded', function displayRamens () {
-    fetch('http://localhost:3000/ramens')
+    fetch('http://localhost:3001/ramens')
     .then(resp => resp.json())
     .then(function(data){
       let imageSection = document.getElementById('ramen-menu')
@@ -11,12 +18,18 @@ function main () {
         imageTag.setAttribute('id',`${object.id}`)
         imageSection.append(imageTag)
       })
+
+      mainImage[0].src = `${data[0].image}`
+      foodName[0].textContent = `${data[0].name}`
+      restaurant[0].textContent = `${data[0].restaurant}`
+      rating.textContent = `${data[0].rating}`
+      comment.textContent = `${data[0].comment}`
     })
   });
 
   function addSubmitListener () {
-    let form = document.getElementById('new-ramen')
-    form.addEventListener('submit', function(e) {
+    let newRamen = document.getElementById('new-ramen')
+    newRamen.addEventListener('submit', function(e) {
       e.preventDefault()
       const configurationObject = {
         method: 'POST',
@@ -25,7 +38,6 @@ function main () {
           'Accept': 'application/json',
         },
         body: JSON.stringify({
-          "id": "6",
           "name": `${e.target[0].value}`,
           "restaurant": `${e.target[1].value}`,
           "image": `${e.target[2].value}`,
@@ -33,7 +45,7 @@ function main () {
           "comment": `${e.target[4].value}`
         })
       }
-      fetch('http://localhost:3000/ramens', configurationObject)
+      fetch('http://localhost:3001/ramens', configurationObject)
       .then(resp => resp.json())
     })
   }
@@ -44,31 +56,63 @@ function main () {
 main()
 
 
+let newArray = []
+
 function handleClick () {
   document.addEventListener('click', function(e) {
-    const select = e.target.closest('img')
-    fetch(`http://localhost:3000/ramens/${select.id}`)
+    let select = e.target.closest('img')
+    newArray.push(select.id)
+    fetch(`http://localhost:3001/ramens/${select.id}`)
     .then(resp => resp.json())
     .then(function(data) {
-      let mainImage = document.getElementsByClassName('detail-image')
       mainImage[0].src = `${data.image}`
-
-      let foodName = document.getElementsByClassName('name')
       foodName[0].textContent = `${data.name}`
-
-      let restaurant = document.getElementsByClassName('restaurant')
       restaurant[0].textContent = `${data.restaurant}`
-
-      let rating = document.getElementById('rating-display')
       rating.textContent = `${data.rating}`
-
-      let comment = document.getElementById('comment-display')
       comment.textContent = `${data.comment}`
     })
   })  
 }
 
 handleClick()
+
+
+function updateRamen () {
+  let editRamen = document.getElementById('edit-ramen')
+  editRamen.addEventListener('submit',function(e){
+    e.preventDefault()
+    rating.textContent = e.target[0].value
+    comment.textContent = e.target[1].value
+    
+    const configurationObject2 = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "rating": `${e.target[0].value}`,
+        "comment": `${e.target[1].value}`
+      })
+    }
+    fetch(`http://localhost:3001/ramens/${newArray[(newArray.length)-1]}`, configurationObject2)
+    .then(resp => resp.json())
+  })
+}
+
+updateRamen()
+
+
+function deleteRamen () {
+  let deleteButton = document.getElementById('delete-button')
+  deleteButton.addEventListener('click', function() {
+    fetch(`http://localhost:3001/ramens/${newArray[(newArray.length)-1]}`,{
+      method: 'DELETE',
+    })
+    .then(resp => resp.json())
+  })
+}
+
+deleteRamen()
 
 
 // Export functions for testing
