d13752b8be40ae6ceac24bbb29470373b2fc02bd
Evan Lee <evan.lee.615@gmail.com>
2024-04-24 01:28:38 -0400

    cleaned up small things, still other small things to clean up

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ let comment = document.getElementById('comment-display')
 
 function main () {
   document.addEventListener('DOMContentLoaded', function displayRamens () {
-    fetch('http://localhost:3001/ramens')
+    fetch('http://localhost:3000/ramens')
     .then(resp => resp.json())
     .then(function(data){
       let imageSection = document.getElementById('ramen-menu')
@@ -24,33 +24,32 @@ function main () {
       restaurant[0].textContent = `${data[0].restaurant}`
       rating.textContent = `${data[0].rating}`
       comment.textContent = `${data[0].comment}`
-    })
-  });
 
-  function addSubmitListener () {
-    let newRamen = document.getElementById('new-ramen')
-    newRamen.addEventListener('submit', function(e) {
-      e.preventDefault()
-      const configurationObject = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json',
-        },
-        body: JSON.stringify({
-          "name": `${e.target[0].value}`,
-          "restaurant": `${e.target[1].value}`,
-          "image": `${e.target[2].value}`,
-          "rating": `${e.target[3].value}`,
-          "comment": `${e.target[4].value}`
-        })
-      }
-      fetch('http://localhost:3001/ramens', configurationObject)
-      .then(resp => resp.json())
-    })
-  }
-
-  addSubmitListener()
+    function addSubmitListener () {
+      let newRamen = document.getElementById('new-ramen')
+      newRamen.addEventListener('submit', function(e) {
+        e.preventDefault()
+        const configurationObject = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+          },
+          body: JSON.stringify({
+            "name": `${e.target[0].value}`,
+            "restaurant": `${e.target[1].value}`,
+            "image": `${e.target[2].value}`,
+            "rating": `${e.target[3].value}`,
+            "comment": `${e.target[4].value}`
+          })
+        }
+        fetch('http://localhost:3000/ramens', configurationObject)
+        .then(resp => resp.json())
+      })
+    }
+    addSubmitListener()
+  })
+})
 }
 
 main()
@@ -62,7 +61,7 @@ function handleClick () {
   document.addEventListener('click', function(e) {
     let select = e.target.closest('img')
     newArray.push(select.id)
-    fetch(`http://localhost:3001/ramens/${select.id}`)
+    fetch(`http://localhost:3000/ramens/${select.id}`)
     .then(resp => resp.json())
     .then(function(data) {
       mainImage[0].src = `${data.image}`
@@ -94,7 +93,7 @@ function updateRamen () {
         "comment": `${e.target[1].value}`
       })
     }
-    fetch(`http://localhost:3001/ramens/${newArray[(newArray.length)-1]}`, configurationObject2)
+    fetch(`http://localhost:3000/ramens/${newArray[(newArray.length)-1]}`, configurationObject2)
     .then(resp => resp.json())
   })
 }
@@ -105,7 +104,7 @@ updateRamen()
 function deleteRamen () {
   let deleteButton = document.getElementById('delete-button')
   deleteButton.addEventListener('click', function() {
-    fetch(`http://localhost:3001/ramens/${newArray[(newArray.length)-1]}`,{
+    fetch(`http://localhost:3000/ramens/${newArray[(newArray.length)-1]}`,{
       method: 'DELETE',
     })
     .then(resp => resp.json())
